a02af9c388a939193d3a43d7a3358b76fbeae1ec
andrea-livornese <andrealivornese@gmail.com>
2024-01-29 17:14:57 +0100

    creazione parziale dello script sasso carta forbici

--- snack/js/script.js
+++ snack/js/script.js
@@ -25,4 +25,50 @@ function media(){
 
 
 
+function playSCF(){
+    //0 sasso
+    //1 carta
+    //2 forbici
+
+
+    let choice; //input
+
+    const cpuChoiceN = Math.floor(Math.random() * max);//veine generato un numero casuale da 0 a 2 che deciderà la scelta della cpu
+    let cpuChoise;
+
+    // convertito il numero generato randomicamente 
+    if(cpuChoiceN == 0){
+        cpuChoise="sasso";
+    }else if(cpuChoiceN == 1){
+        cpuChoise="carta";
+    }else{
+        cpuChoise="forbici";
+    }
+
+    //caso l'utente scelga sasso
+    if(choice == cpuChoise){
+        // pareggio
+    }else if(choise == "sasso" && cpuChoise =="carta"){
+        // perdita
+    }else if(choise == "sasso" && cpuChoise =="forbici"){
+        // vincita
+    }
+
+    //caso l'utente scelga carta
+    if(choise == "carta" && cpuChoise =="forbici"){
+        // perdita
+    }else if(choise == "carta" && cpuChoise =="sasso"){
+        // vincita
+    }
+
+    //caso l'utente scelga forbici
+    if(choise == "forbici" && cpuChoise =="sasso"){
+        // perdita
+    }else if(choise == "forbici" && cpuChoise =="carta"){
+        // vincita
+    }
+}
+
+
+
     
