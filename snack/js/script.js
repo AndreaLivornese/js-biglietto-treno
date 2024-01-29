64eda75a1a17914ebd3632a1afeaddf82459f945
andrea-livornese <andrealivornese@gmail.com>
2024-01-29 22:53:29 +0100

    creazione grafica e completamento dello script sasso carta forbici

--- snack/js/script.js
+++ snack/js/script.js
@@ -33,41 +33,43 @@ function playSCF(){
     //2 forbici
 
 
-    let choice; //input
+    let choice= document.getElementById("scelta-utente").value;
 
-    const cpuChoiceN = Math.floor(Math.random() * max);//veine generato un numero casuale da 0 a 2 che deciderà la scelta della cpu
-    let cpuChoise;
+    const cpuChoiceN = Math.floor(Math.random() * 3);//veine generato un numero casuale da 0 a 2 che deciderà la scelta della cpu
+    let cpuChoice;
 
     // convertito il numero generato randomicamente 
     if(cpuChoiceN == 0){
-        cpuChoise="sasso";
+        cpuChoice="sasso";
     }else if(cpuChoiceN == 1){
-        cpuChoise="carta";
+        cpuChoice="carta";
     }else{
-        cpuChoise="forbici";
+        cpuChoice="forbici";
     }
 
+    document.getElementById("gioco").innerHTML="l'avversario ha scelto " + cpuChoice + "<br>";
+
     //caso l'utente scelga sasso
-    if(choice == cpuChoise){
-        // pareggio
-    }else if(choise == "sasso" && cpuChoise =="carta"){
-        // perdita
-    }else if(choise == "sasso" && cpuChoise =="forbici"){
-        // vincita
+    if(choice == cpuChoice){
+        document.getElementById("result").innerHTML="E' un pareggio."
+    }else if(choice == "sasso" && cpuChoice =="carta"){
+        document.getElementById("result").innerHTML="Hai perso."
+    }else if(choice == "sasso" && cpuChoice =="forbici"){
+        document.getElementById("result").innerHTML="Hai vinto."
     }
 
     //caso l'utente scelga carta
-    if(choise == "carta" && cpuChoise =="forbici"){
-        // perdita
-    }else if(choise == "carta" && cpuChoise =="sasso"){
-        // vincita
+    if(choice == "carta" && cpuChoice =="forbici"){
+        document.getElementById("result").innerHTML="Hai perso."
+    }else if(choice == "carta" && cpuChoice =="sasso"){
+        document.getElementById("result").innerHTML="Hai vinto."
     }
 
     //caso l'utente scelga forbici
-    if(choise == "forbici" && cpuChoise =="sasso"){
-        // perdita
-    }else if(choise == "forbici" && cpuChoise =="carta"){
-        // vincita
+    if(choice == "forbici" && cpuChoice =="sasso"){
+        document.getElementById("result").innerHTML="Hai perso."
+    }else if(choice == "forbici" && cpuChoice =="carta"){
+        document.getElementById("result").innerHTML="Hai vinto."
     }
 }
 
